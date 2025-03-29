d0773026f0b450d2993e4665f3e19330d2c52f42
CUHP <phuc260706@gmail.com>
2025-03-29 09:16:47 +0700

    update

--- Assets/_Scripts/Manager/SoundManager.cs
+++ Assets/_Scripts/Manager/SoundManager.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using static SoundManager;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static SoundManager Instance {  get; private set; }
+    [SerializeField] private AudioSource _audioSource;
+    [Header("Volume Settings")]
+    [Range(0f, 1f)] public float musicVolume = 1f;
+    [Range(0f, 1f)] public float effectsVolume = 1f;
+    [SerializeField] private List<AudioClip> _sfx;
+    [SerializeField] private List<AudioClip> _music;
+    [SerializeField] private Slider music;
+    [SerializeField] private Slider sound;
+    private void Awake()
     {
-        
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void PlayButtonSound()
     {
-        
+        _audioSource.clip = _sfx[(int)SFX.Button];
+        _audioSource.Play();
+    }
+    public void PlaySFX(SFX sFX) 
+    {
+        switch(sFX)
+        {
+            case(SFX.Attack):
+
+                break;
+
+            case (SFX.Correct):
+
+                break;
+
+            case (SFX.Button):
+                _audioSource.clip = _sfx[(int)sFX];
+                _audioSource.Play();
+                break;
+            case (SFX.None):
+                
+                break;
+        }
+    }
+    public void PlayMusic()
+    {
+
+    }
+    public enum SFX
+    {
+        None,
+        Attack,
+        Correct,
+        Button,
     }
 }
